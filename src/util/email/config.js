59033d22a2336cd23ecf4e6a7ed7ac098a907a9f
Hugh Caluscusin (Dev) <hugh@melodiccrypter.com>
2020-04-27 03:19:11 +0800

    modified

--- src/util/email/config.js
+++ src/util/email/config.js
@@ -6,6 +6,12 @@ const transport = nodemailer.createTransport({
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
     },
+    // host: process.env.MAIL_HOST,
+    // port: process.env.MAIL_PORT,
+    // auth: {
+    //     user: process.env.MAIL_USER,
+    //     pass: process.env.MAIL_PASS,
+    // },
 });
 
 export default transport;
